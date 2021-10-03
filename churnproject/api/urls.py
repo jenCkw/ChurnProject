df4887c591b14b66d0b1af22a683bf8fdd5d2b5e
graceDev1 <grace.devolop@gmail.com>
2021-10-03 12:48:24 +0200

    service crud operation

--- churnproject/api/urls.py
+++ churnproject/api/urls.py
@@ -10,5 +10,13 @@ urlpatterns = [
     path('partner/', views.getPartner),
     path('partner/create/', views.createPartner),
     path('partner/put/<str:pk>/',views.updatePartner ),
-    path('partner/delete/<str:pk>/',views.deletePartner)
+    path('partner/delete/<str:pk>/',views.deletePartner),
+    path('TypeService/', views.getAllTypeService),
+    path('TypeService/create/', views.createTypeService),
+    path('TypeService/put/<str:pk>/',views.updateTypeService ),
+    path('TYpeService/delete/<str:pk>/',views.deleteTypeService ),
+    path('Service/', views.getAllService),
+    path('Service/create/',views.createService),
+    path('Service/put/<str:pk>/',views.updateService),
+    path('Service/delete/<str:pk>/',views.deleteService ) 
 ]
--- churnproject/api/views.py
+++ churnproject/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
-from .serializers import CustomerSerializer, PartnerSerializer
-from .models import Customer, Partner
+from .serializers import CustomerSerializer, PartnerSerializer, ServiceSerializer, TypeServiceSerializer
+from .models import Customer, Partner, Service, TypeService
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -115,3 +115,74 @@ def deletePartner(request,pk):
     partner = Partner.objects.get(id=pk)
     partner.delete()
     return Response("partner deleted")
+
+
+#============= service endpoint=========
+
+@api_view(['GET'])
+def getAllService(request):
+    service = Service.objects.all()
+    serializer = ServiceSerializer(service, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(["POST"])
+def createService(request):
+    data = request.data
+    service = Service.objects.create(name=data['name'], type=data['type'])
+    serializer = ServiceSerializer(service, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(["PUT"])
+def updateService(request,pk):
+    data = request.data
+    service= Service.objects.get(id=pk)
+    serializer = ServiceSerializer(service, data=data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+
+@api_view(["DELETE"])
+def deleteService(request,pk):
+    service = Service.objects.get(id=pk)
+    service.delete()
+    return Response("service deleted")
+
+
+#============= service endpoint=========
+@api_view(['GET'])
+def getAllTypeService(request):
+    typService = TypeService.objects.all()
+    serializer = TypeServiceSerializer(typService, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(["POST"])
+def createTypeService(request):
+    data = request.data
+    typeService = TypeService.objects.create(name=data['name'], price=data['price'])
+    serializer = TypeServiceSerializer(typeService, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def updateTypeService(request,pk):
+    data = request.data
+    typService = TypeService.objects.get(id=pk)
+    serializer = TypeServiceSerializer(typService, data=data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(["DELETE"])
+def deleteTypeService(request,pk):
+    typService = TypeService.objects.get(id=pk)
+    typService.delete()
+    return Response("custumer deleted")
+
