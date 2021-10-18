c2d34351fe027fc300a2f6c2c5fb04752b18bf66
graceDev1 <grace.devolop@gmail.com>
2021-10-18 13:45:12 +0200

    report print

--- client/src/components/customerInfo/CustomerInfo.tsx
+++ client/src/components/customerInfo/CustomerInfo.tsx
@@ -1,5 +1,8 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { useChurnQuery } from '../../generated/graphql';
+import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
+import generatePDf from "../../services/reportGenerator";
+
 
 function CustomerInfo() {
 
@@ -72,6 +75,30 @@ function CustomerInfo() {
         },
     ]
 
+    const columnsData = ["ID", "customerID","Gender","partner","seniorCitizen","tenure","Contract","PaymentMethod","TotalCharges","Churn"]
+
+    const churnsData:any = []
+ 
+
+    const churns = data?.churn.forEach((churn)=>{
+        const churnData = [
+            churn.id,
+            churn.customerID,
+            churn.gender,
+            churn.partner,
+            churn.seniorCitizen,
+            churn.tenure,
+            churn.Contract,
+            churn.PaymentMethod,
+            churn.TotalCharges,
+            churn.churn
+        ]
+        churnsData.push(churnData)
+    })
+
+    console.log(churns)
+    console.log(churnsData)
+
     if(!data) return <div className="workflow  p-4"><h1 className="text-center">No Data...</h1></div>
     if(error) return <div className="workflow text-center p-4"><h1 className="text-center">Error... </h1></div>
     if(loading) return <div className="workflow text-center p-4"><h1 className="text-center">Loading...</h1></div>
@@ -81,7 +108,14 @@ function CustomerInfo() {
             <div className="bg-white p-4 m-2">
                <h5 className="font-weight-bold text-center">Customer Info</h5>
            </div>
+           
            <div className="p-2 bg-white m-2" >
+            <div className="m-2 justify-content-end">
+                        <button onClick={()=> generatePDf(columnsData,churnsData)} className="btn btn-secondary">
+                            print {"  "}
+                        <LocalPrintshopIcon style={{cursor:'pointer', float:'right'}} className=" text-primary"/>
+                        </button>
+                </div>
            <div style={{ height: 400, width: '100%' }}>
             <DataGrid
                 rows={data.churn}
--- client/src/components/user/User.tsx
+++ client/src/components/user/User.tsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { useDeleteUserMutation, useUsersQuery } from "../../generated/graphql"
 import { DataGrid } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
+import SearchBar from "material-ui-search-bar";
+import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
+import generatePDf from "../../services/reportGenerator";
+
 
 function User() {
     const {data, loading, error} = useUsersQuery({fetchPolicy:"network-only"}); 
+    const [rows, setRows] = useState(data?.users);
+    const [searched, setSearched] = useState('');
     const [deleteOneUser] = useDeleteUserMutation();
+
+
+    //makes search to users data
+    const requestSearch = (searchedVal: string) => {
+        const filteredRows = data?.users.filter((row) => {
+          return row.name.toLowerCase().includes(searchedVal.toLowerCase());
+        });
+        setRows(filteredRows);
+      };
+
+
+      const cancelSearch = () => {
+        setSearched("");
+        requestSearch(searched);
+      };
+
+    // delete user 
     const deleteUser = (id : number) =>{
         deleteOneUser({
             variables:{
@@ -16,6 +40,21 @@ function User() {
         })
     }
 
+   const columnsPdf= ["ID","NAME","EMAIL"]
+   
+   const usersData:any = []
+
+   const users:any = data?.users.forEach((user)=>{
+       const userData = [
+           user.id,
+           user.name,
+           user.email
+       ]
+
+       usersData.push(userData)
+   })
+
+    // define columns
     const columns = [
         {
             field:"id",
@@ -51,6 +90,8 @@ function User() {
     if(error) return <div className="workflow">error</div>
     if(loading) return <div className="workflow">Loading.....</div>
 
+    console.log(users)
+    console.log(usersData)
     return (
         <div className="workflow p-4">
            <div className="bg-white p-4 m-2 d-flex justify-content-between">
@@ -61,16 +102,31 @@ function User() {
                     </Link>
                 </div>
            </div>
-           <div className="p-2 bg-white m-2">
-           <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-                rows={data?.users!}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
+           <div className="bg-white p-4 m-2">
+            <SearchBar
+                    value={searched}
+                    onChange={(searchVal) => requestSearch(searchVal)}
+                    onCancelSearch={() => cancelSearch()}
             />
-    </div>
+           </div>
+           <div className="p-2 bg-white m-2">
+                <div className="m-2 justify-content-end">
+                    <button onClick={()=> generatePDf(columnsPdf,usersData)} className="btn btn-secondary">
+                        print {"  "}
+                    <LocalPrintshopIcon style={{cursor:'pointer', float:'right'}} className=" text-primary"/>
+                    </button>
+               </div>
+            <div style={{ height: 400, width: '100%' }} id="data">
+                <DataGrid
+                    rows={!rows ? data.users! : rows!}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+                
+            </div>
+           
            </div>
         </div>
     )
